da6fa472957d74fc91291699274b724b21440b70
Priyanshu Gupta <priyanshu16095@gmail.com>
2024-03-04 11:13:49 +0530

    Added sortDict function

--- 04-03-2024.py
+++ 04-03-2024.py
@@ -1,1 +1,7 @@
-print("Hello world")
+def sortDict(dict1):
+    dict2 = dict(sorted(dict1.items(), key=lambda item: item[1], reverse=True))
+    return dict2
+
+dict1 = {'stu1': 85, 'stu2': 92, 'stu3': 78, 'stu4': 95, 'stu5': 89}
+dict2 = sortDict(dict1)
+print(dict2)
