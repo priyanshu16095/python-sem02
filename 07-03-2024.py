6abfc85fd2783a0748e5b98003c3b0f1d16c8471
Priyanshu Gupta <priyanshu16095@gmail.com>
2024-03-07 11:35:14 +0530

    added filter program

--- 07-03-2024.py
+++ 07-03-2024.py
@@ -1,5 +1,5 @@
 num1 = 10
-num2 = 5
+num2 = 0
 try:
     result = num1 / num2
 except ZeroDivisionError:
@@ -9,4 +9,31 @@ except Exception:
 else:
     print(result)
 finally:
-    print("No exception occured :)")
+    print("Thank you! :)")
+
+nums = [10, 101, 12, 14, 15, 18]
+
+def sumOfDigits(nums):
+    result = []
+    for i in nums:
+        sum = 0 
+        while(i > 0):
+            digit = i % 10
+            sum += digit
+            i = (int) (i / 10)
+        result.append(sum)
+    return result
+
+result = sumOfDigits(nums)
+print(result)
+
+def checkSum(nums):
+    temp = sumOfDigits(nums)
+    for i in temp:
+        if i == 10:
+            return True
+        else:
+            return False
+
+arr = list(filter(checkSum, nums))
+print(arr)
