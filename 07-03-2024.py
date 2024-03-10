de29474e05625e5735408e99c76d4bcee07a40c3
Priyanshu Gupta <priyanshu16095@gmail.com>
2024-03-10 18:25:13 +0530

    Some minor changes

--- 07-03-2024.py
+++ 07-03-2024.py
@@ -11,29 +11,34 @@ else:
 finally:
     print("Thank you! :)")
 
-nums = [10, 101, 12, 14, 15, 18]
+list1 = [10, 101, 12, 14, 15, 18]
 
 def sumOfDigits(nums):
     result = []
     for i in nums:
-        sum = 0 
-        while(i > 0):
+        current_sum = 0
+        while i > 0:
             digit = i % 10
-            sum += digit
-            i = (int) (i / 10)
-        result.append(sum)
+            current_sum += digit
+            i = int(i / 10)
+        result.append(current_sum)
     return result
-
-result = sumOfDigits(nums)
-print(result)
 
 def checkSum(nums):
     temp = sumOfDigits(nums)
     for i in temp:
-        if i == 10:
-            return True
-        else:
+        if i != 10:
             return False
+    return True
 
-arr = list(filter(checkSum, nums))
+arr = list(filter(checkSum, list1))
 print(arr)
+
+def isVowel(a):
+    v = ['a', 'e', 'i', 'o', 'u']
+    for i in a:
+        for j in v:
+            if i == j:
+                return True
+alphas = ['a', 'b', 'e', 'f', 'o', 'm', 'n', 'u']
+vowels = list(filter(isVowel, alphas))
---
+++ rough.py
@@ -0,0 +1,5 @@
+total = 0
+while total < 50:
+    num = int(input("Enter the number: "))
+    total += num
+print(total)
